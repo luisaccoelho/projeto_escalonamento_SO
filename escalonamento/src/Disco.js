f1f61afe8041296f97476ed10d0af07242d3ebff
unknown <luisacoelho@ufba.br>
2023-11-14 18:07:35 -0300

    Classe Disco adicionada

---
+++ escalonamento/src/Disco.js
@@ -0,0 +1,12 @@
+import Process from "./Process";
+import Fila from "./Fila";
+
+export default class Disco {
+    constructor(processos = []){
+        this._processos = new Fila(processos);
+    }
+
+    atualizaDisco(t){
+        this._processos.chegam(t);
+    }
+}
