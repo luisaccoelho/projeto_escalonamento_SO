6ad660f3423ce82f5596cf4061dfd23a5b9bcc3c
Luciara Regina Piauí de Oliveira <luciara@MacBook-Air-de-Luciara.local>
2023-11-15 17:35:49 -0300

    funcionamento dos algoritmos atualizado

--- escalonamento/src/memoria/Ram.js
+++ escalonamento/src/memoria/Ram.js
@@ -1,6 +1,6 @@
 export default class Ram{
     constructor(processos=[],fila=[],t=0,algoritmo){//O t é o tempo atual da simulação
-        this._fila = fila;//Estado atual da fila
+        this._ram = fila;//Estado atual da RAM
         this._processos = processos;//Todos os processos que ainda não chegaram
         if(t===0){//Se o tempo é igual a 0, cria a fila do 0 e já adiciona os processos que chegam no tempo 0 à fila
             this.chegam(0);
@@ -10,41 +10,46 @@ export default class Ram{
         this._algoritmo = algoritmo;
     }
 
-    entra(processo){//Adiciona um processo na RAM, caso não haja espaço chama sai(processo) onde processo é escolhido a partir do algoritmo em vigor
+    entra(processo){//Adiciona um processo na RAM, caso não haja espaço sobrescreve de acordo com o algoritmo em vigor
         if(this._tamanho-this._ocupado>=processo.tamamho) {//Um processo só é adicionado caso haja espaço para todas as suas páginas
-            this._fila.push(processo);
+            this._ram.push(processo); //Se há espaço, apenas coloca o processo no final da fila
+            processo.enderecoRam = this._ram.length-1;
             this._ocupado += processo.tamamho;
         } else {
             switch (this._algoritmo){
                 case 'FIFO':
-                    this._fila.shift(); //Tira o processo que entrou há mais tempo (o primeiro da fila)
-                    this.entra(processo);
-                    return this._fila;
-                case 'MRU':
-                    let menosRecentementeUtilizado = this._fila[0];
-                    for(let i=0;i<this._fila.length;i++){
-                        if(this._fila[i].ultimaChamada<menosRecentementeUtilizado){ //Procura o processo chamado a menos tempo
-                            menosRecentementeUtilizado = this._fila[i];
+                    let primeiro = this._ram[0]
+                    for(let i=0;i<this._ram.length;i++){
+                        if(this._ram[i].tempoChegada<primeiro.tempoChegada){
+                            primeiro = this._ram[i];
                         }
                     }
-                    this.sai(menosRecentementeUtilizado); //Retira o processo utilizado a menos tempo
-                    this.entra(processo);
-                    return this._fila;
+                    this.substitui(primeiro, processo);//Tira o processo que entrou há mais tempo (o primeiro da fila)
+                    return this._ram;
+                case 'MRU':
+                    let menosRecentementeUtilizado = this._ram[0];
+                    for(let i=0;i<this._ram.length;i++){
+                        if(this._ram[i].ultimaChamada<menosRecentementeUtilizado){ //Procura o processo chamado a menos tempo
+                            menosRecentementeUtilizado = this._ram[i];
+                        }
+                    }
+                    this.substitui(menosRecentementeUtilizado, processo); //Retira o processo utilizado a menos tempo
+                    return this._ram;
             } 
         }
-        return this._fila;
+        return this._ram;
     }
 
-    sai(processo){//Remove processo da fila
-        if(this._fila.length===0){//Se a fila está vazia, retorna null
+    substitui(processoAntigo, processoNovo){//Remove processo da fila
+        if(this._ram.length===0){//Se a fila está vazia, retorna null
             return null;
         }
-        for(let i=0;i<this._fila.length;i++){
-            if(this._fila[i]===processo){
-                this._fila.splice(i, 1);
+        for(let i=0;i<this._ram.length;i++){
+            if(this._ram[i]===processoAntigo){
+                this._ram.splice(i, 1, processoNovo);
             }
         }
-        return this._fila;
+        return this._ram;
     }
 
     atualizaRam(t){//Adiciona no fim da fila todos os processos que chegam no tempo t (na ordem em que aparecem no array de processos)
