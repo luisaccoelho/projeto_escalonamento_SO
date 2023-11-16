1639910d76f16b6f5e3d262019ad2fa1391c2ff2
LucasTBorges <lucastborges@gmail.com>
2023-11-16 18:25:36 -0300

    Correção na visualização

--- escalonamento/src/App.js
+++ escalonamento/src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import Coluna from './debug/colunaTeste.js';
 
 function App() {
-  //let colunaA = [Tabela.ACHEGAR, Tabela.EXECUTANDO, Tabela.ESPERANDO, Tabela.FINALIZADO, Tabela.SOBRECARGA, Tabela.EXECUTANDO_DL, Tabela.ESPERANDO_DL, Tabela.FINALIZADO_DL];
+  let colunaA = [Tabela.ACHEGAR, Tabela.EXECUTANDO, Tabela.ESPERANDO, Tabela.FINALIZADO, Tabela.SOBRECARGA, Tabela.EXECUTANDO_DL, Tabela.ESPERANDO_DL, Tabela.FINALIZADO_DL];
   let simulacao = new Simulacao(Algoritmo.RR, [new Process(0,2,4), new Process(1,0,3), new Process(2,1,2), new Process(3,3,1)]);
   let i = 0;
   while(!simulacao.terminou()&&i<100){
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Coluna coluna={simulacao.colunas} />
+        <Coluna coluna={colunaA} />
       </header>
     </div>
   );
